1f238c1418f6fd33510a06a9c505456a1074b021
Dan <2435601897@qq.com>
2024-02-15 16:34:48 +0800

    Test

---
+++ Lesson/Hellocsharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Hellosharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        //时间：2023/12/10 20:49
+        //内容：C#begin
+        //心情：一般
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello csharp!");
+            #region 
+            //Console.ReadLine();
+            #endregion
+            //字符型变量
+            //char b ='a';
+            //短整型变量
+            //short d =30;
+            //单精度浮点型变量；
+            //float c=6 ;
+            //int a =10;
+            //int b=3;
+            //双精度浮点型变量；
+            //double e=2.1234;
+            //Console.WriteLine(a%b);
+            //Console.WriteLine("请输入一个数字");
+            //string value= Console.ReadLine();
+            //Console.WriteLine("您输入的数字是"+value + ",这将是你的幸运数字!");
+            //任意输入两个数，计算和差积商
+            //Console.WriteLine("请输入一个数字");
+            //int a = Convert.ToInt32(Console.ReadLine()); 
+            //Console.WriteLine("请输入二个数字");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("这两个数的和差积商是,"+(a + b)+","+(a - b)+ "," + (a * b)+ "," + (a / b));
+            //交换两个int类型的值
+            //比较Console.ReadLine和Console.Read的区别
+
+
+
+
+        }
+    }
+}
